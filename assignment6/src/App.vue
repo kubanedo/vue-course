027c31a14df783a178c1895ac5e241b0acb65d35
jack7cz <prerost.j@seznam.cz>
2020-03-24 14:55:37 +0100

    init

---
+++ assignment6/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <app-header></app-header>
+        <app-body></app-body>
+        <app-footer></app-footer>
+    </div>
+</template>
+
+<script>
+import Header from './Header.vue'
+import Body from './Body.vue'
+import Footer from './Footer.vue'
+
+export default {
+  components: {
+            'app-header': Header,
+            'app-body': Body,
+            'app-footer': Footer
+  }
+}
+</script>
+
+<style>
+
+</style>
