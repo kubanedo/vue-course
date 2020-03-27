39ad5541badb60a19b8ded805db00724966ba26d
jack7cz <prerost.j@seznam.cz>
2020-03-27 12:36:39 +0100

    assignment8

---
+++ assignment8/src/components/Green.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h3>Green component</h3>         
+        <slot></slot>        
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+    div {
+        border: 1px solid green;
+        background-color: lightgreen;
+        padding: 30px;
+        margin: 20px auto;
+        text-align: center
+    }
+</style>
